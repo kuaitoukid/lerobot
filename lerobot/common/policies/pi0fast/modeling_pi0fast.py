9307f34424161411ad8fec4d7885847b9af2b9ae
wenhao.he <wenhao.he@noematrix.cn>
2025-08-27 14:10:42 +0800

    fix ignore_index for latest transformers

--- lerobot/common/policies/pi0fast/modeling_pi0fast.py
+++ lerobot/common/policies/pi0fast/modeling_pi0fast.py
@@ -472,7 +472,7 @@ class PI0FAST(nn.Module):
                 param.data = param.data.to(dtype=torch_precision)
         self.set_requires_grad()
         self.image_keys = self.config.image_features.keys()
-        self.ignore_index = self.pi0_paligemma.config.ignore_index
+        self.ignore_index = -100  # self.pi0_paligemma.config.ignore_index
         self.padding_side = self.config.padding_side
 
     def set_requires_grad(self):
